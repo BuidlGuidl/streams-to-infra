42aec9b11e204092c175c06e0e02effedfc69de4
Austin Griffith <austin@ethereum.org>
2023-08-07 14:20:10 -0600

    swap the order

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -13,13 +13,13 @@ export const Header = () => {
           <Image src="/jessy.png" alt="Hacker House Logo" width={523} height={36} className="mx-4" />
           <div className="m-0 mt-1 pl-4 text-sm md:text-base">
             <span className="pr-2">selection committee:</span>
-            <a href="https://twitter.com/13yearoldvc" target="_blank" rel="noreferrer">
-              Jessy
-            </a>{" "}
-            and{" "}
             <a href="https://github.com/0xJepsen" target="_blank" rel="noreferrer">
               Waylon
             </a>
+            and{" "}
+            <a href="https://twitter.com/13yearoldvc" target="_blank" rel="noreferrer">
+              Jessy
+            </a>{" "}
           </div>
         </div>
       </div>
